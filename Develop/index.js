88b9c739773babf13bdd63171041cc2a73e564a2
bkhandag <khandagale.b.g@gmail.com>
2022-04-21 17:52:11 -0700

    created quesitons array, writeToFile and init funciton

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of our project?',
+        name: 'projectTitle',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?', 
+      },
+      {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'What is the Table of Contents?',
+      },
+      {
+          type: 'input',
+          name: 'installationInstructions',
+          message: 'What are the installation instructions',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'What is Usage?',
+        },
+        {
+          type: 'list', //TODO Change this to list
+          name: 'license',
+          message: 'What License would you like to add',
+          choices: [
+              'Apache_License_2.0',
+              'MIT_License',
+              'GNU_General_Public_License',
+              'None',
+          ]
+        },
+        {
+          type: 'input',
+          name: 'contribution',
+          message: 'Who contributed to the project?',
+        },
+        {
+          type: 'input',
+          name: 'tests',
+          message: 'What tests were run?',
+        },
+        {
+          type: 'input',
+          name: 'questions',
+          message: 'What are your questions?',
+        },
+  
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(path.join(process.cwd(), fileName), data, function(err){
+        err ? console.error(err) : console.log('Commit logged!')});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt(questions)
+  .then((response) => {
+      
+
+    console.log("Now generating your readme.md ...");
+    writeToFile("/dist/README.md", generateMarkdown(response))
+
+
+
+
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,92 +1,54 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![License](https://img.shields.io/badge/license-${license}-red.svg)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `\n [License](#license)`
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License
+  This project uses the ${license} license
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is the title of our project?',
-      name: 'projectTitle',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'What is the description of your project?', 
-    },
-    {
-      type: 'input',
-      name: 'tableOfContents',
-      message: 'What is the Table of Contents?',
-    },
-    {
-        type: 'input',
-        name: 'installationInstructions',
-        message: 'What are the installation instructions',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'What is Usage?',
-      },
-      {
-        type: 'input',
-        name: 'license',
-        message: 'What is License',
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'Who contributed to the project?',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'What tests were run?',
-      },
-      {
-        type: 'input',
-        name: 'questions',
-        message: 'What are your questions?',
-      },
 
-  ])
-  .then((response) => {
-      //The following command shows how to write fil
+  return `# readme-generator ${data.projectTitle}
 
-    console.log(response);
-    const bio = `<!DOCTYPE html>
-    <html lang="en">
-    <head> </head>
-    <body> 
-    <p> Hi my name is ${response.name}. I live in ${response.location}. 
-    I currently do ${response.bio} 
-    </p>
-    </body>
-    </html>`
+  ${renderLicenseBadge(data.license)}
 
-    fs.writeFile("portfolio.html", bio, function(err){
-          err ? console.error(err) : console.log('Commit logged!')});
-
-  });
-
-
-  return `# ${data.title}
-
-`;
+  ## Description:
+  ${data.description}
+  
+  ## Table of Contents:
+  ${data.tableOfContents}
+  ${renderLicenseLink(data.license)}
+  
+  ## Installation Instructions:
+  ${data.installationInstructions}
+  
+  ## Usage:
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contribution:
+  ${data.contribution}
+  
+  ## Tests:
+  ${data.tests}
+  
+  ## Questions:
+  ${data.questions}`
 }
 
 module.exports = generateMarkdown;
